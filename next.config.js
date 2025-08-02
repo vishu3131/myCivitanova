5f61898ffc32792abbd293e625ab83bfc74c383b
vishu <vishusingh2002@gmail.com>
2025-08-02 05:08:19 +0200

    fix: Rimuove la configurazione webpack per risolvere l'errore di Vercel

--- next.config.js
+++ next.config.js
@@ -5,37 +5,6 @@ const nextConfig = {
   },
   // Assicura che i CSS siano processati correttamente
   swcMinify: true,
-  webpack: (config, { isServer }) => {
-    // Risolve i problemi con i moduli WebSocket in ambiente browser
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-        crypto: false,
-        stream: false,
-        url: false,
-        zlib: false,
-        http: false,
-        https: false,
-        assert: false,
-        os: false,
-        path: false,
-        bufferutil: false,
-        'utf-8-validate': false,
-      };
-    }
-
-    // Ignora i warning per i moduli opzionali
-    config.ignoreWarnings = [
-      { module: /node_modules\/ws\/lib\/buffer-util\.js/ },
-      { module: /node_modules\/ws\/lib\/validation\.js/ },
-      { module: /node_modules\/@supabase\/realtime-js/ },
-    ];
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
