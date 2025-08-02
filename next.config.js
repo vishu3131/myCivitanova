48825cb109b3981d083adb1509a6e26bdd5a1a15
vishu <vishusingh2002@gmail.com>
2025-08-02 05:12:23 +0200

    fix: Rimuove la configurazione experimental da next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    esmExternals: 'loose',
-  },
   // Assicura che i CSS siano processati correttamente
   swcMinify: true,
 };
