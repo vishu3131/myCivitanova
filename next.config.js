cf055bbebeb95f68115aeee5a400226fe414706d
vishu3131 <vishusingh2002@gmail.com>
2025-08-02 13:17:25 +0200

    Fix Unsplash hostname configuration with wildcard

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'source.unsplash.com',
+        hostname: '*.unsplash.com',
         pathname: '/**',
       },
     ],
